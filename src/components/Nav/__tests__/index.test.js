8393cf06c5a06270cef6ec8f10c224c1175c52bd
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-19 16:44:44 -0400

    set up tests for Gallery and Photo

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,11 +3,23 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+  {name: 'portraits', description: 'Portraits of peole in my life'}
+]
+
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
+// can't get matches snapshot to work with useEffects, which isn't in the snapshot code at the end of the module
 describe('Nav component', () => {
   it('renders', () => {
-    render(<Nav />);
+    render(<Nav 
+      categores={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+    />);
   });
 
   it('matches snapshot', () => {
@@ -17,21 +29,27 @@ describe('Nav component', () => {
   });
 })
 
-// doesn't like destructuring, but the tests still run?
+// // doesn't like destructuring, but the tests still run?
 // describe('emoji is visible', () => {
 //   it('inserts emoji into the h2', () => {
-//   const { getByLabelText } = render(<Nav />);
+//   const { getByLabelText } = render(<Nav 
+//     categores={categories}
+//     setCurrentCategory={mockSetCurrentCategory}
+//     currentCategory={mockCurrentCategory}
+//   />);
 
 //   expect(getByLabelText('camera')).toHaveTextContent('📸');
 //   });
 // })  
 
-// describe('links are visible', () => {
+// // describe('links are visible', () => {
 //   it('inserts text into the links', () => {
-//     const { getByTestId } = render(<Nav />);
+//     const { getByTestId } = render(<Nav 
+//       categores={categories}
+//       setCurrentCategory={mockSetCurrentCategory}
+//       currentCategory={mockCurrentCategory}
+//     />);
 
 //     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
 //     expect(getByTestId('about')).toHaveTextContent('About me');
 //   });
-
-// })
