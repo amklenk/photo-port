b19f0ea05e2d0867971ef7f48e371c35ed584f1e
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-20 10:27:07 -0400

    create contact form and functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ContactForm(){
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+
+    const { name, email, message } = formState;
+
+    function handleChange(event) {
+        // use spread operator to retain the other key-value pairs in the object
+        setFormState({ ...formState, [event.target.name]: event.target.value })
+    }
+
+    // lets us see that the formState was updated properly
+    // console.log(formState);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+      }
+      
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id='contact-form'>
+            <form id='contact-form' onSubmit={handleSubmit}>
               <div>
                 <label htmlFor='name'>Name:</label>
-                <input type='text' name='name'/>
+                <input type='text' onChange={handleChange} name='name' defaultValue={name}/>
               </div>
               <div>
                 <label htmlFor='email'>Email address:</label>
-                <input type='email' name='email'/>
+                <input type='email' onChange={handleChange} name='email' defaultValue={email} />
               </div>
               <div>
                 <label htmlFor='message'>Message:</label>
-                <textarea name='message' rows='5'/>
+                <textarea name='message' onChange={handleChange} defaultValue={message} rows='5'/>
               </div>
               <button type='submit'>Submit</button>
             </form>
